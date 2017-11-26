dc4ef597e58858b88bef7691203b2e8ad9c622b1
Omayeli Arenyeka <aomayeli@gmail.com>
2017-11-26 11:02:44 -0800

    Fetching from spreadsheet

--- president.js
+++ president.js
@@ -1,39 +1,49 @@
-var restclient = require('node-rest-client');
+var restclient = require('node-rest-client').Client;
+var client = new restclient();
 var Twit = require('twit');
 var GoogleSpreadsheet = require('google-spreadsheet');
 var async = require('async');
-
-// Twitter app info
-var T = new Twit(require('./config.js'));
 var app = require('express')();
 var path = require('path');
 
-var doc = new GoogleSpreadsheet('1yQ_Ff6iqK3ANekxOLr6RgOC9EDbiA8BMXsu6YVxVd7w');
+// Twitter app info
+var T = new Twit(require('./config.js'));
 
-// I deployed to Nodejitsu, which requires an application to respond to HTTP requests
-// If you're running locally you don't need this, or express at all.
+// Data stuff 
+var doc = new GoogleSpreadsheet('1yQ_Ff6iqK3ANekxOLr6RgOC9EDbiA8BMXsu6YVxVd7w');
+var corporaBaseURL = 'https://botwiki.org/api/corpora/data/';
+var corporaData = ['games/pokemon.json', 'film-tv/tv_shows.json', 'science/planets.json', 'words/nouns.json', 'objects/objects.json', 'humans/celebrities.json'];
+
+
 app.get('/', function(req, res){
-  //res.sendfile('index.html');
   res.sendFile(path.resolve('index.html'));
 });
 
 app.listen(3000);
 
-var corporaBaseURL = 'https://botwiki.org/api/corpora/data/';
-var corporaData = ['games/pokemon.json', 'film-tv/tv_shows.json', 'science/planets.json', 'words/nouns.json', 'objects/objects.json', 'humans/celebrities.json'];
 
-var statement =   "";
+var statement = 'My president is';
 
 function makeSentence() {
-  console.log('in here');
-  statement = "My president is";
-
- doc.getInfo(function(err, info) {
-    console.log(info);
-    // sheet = info.worksheets[0];
-    // console.log('sheet 1: '+sheet.title+' '+sheet.rowCount+'x'+sheet.colCount);
-    // step();
-  });
+  var num = Math.floor(Math.random() * corporaData.length+1);
+  if (num > corporaData.length) {
+    doc.getInfo(function(err, info) {
+      sheet = info.worksheets[0];
+      sheet.getRows({
+        offset: 1,
+        limit: 20,
+        orderby: 'col2'
+      }, function( err, rows ){
+        console.log('Read '+rows.length+' rows');
+      });
+    });
+  }
+  else {
+    var url = corporaBaseURL + corporaData[num];
+    client.get(url, function (data, response) {
+      console.log(data);
+    });
+  }
 }
 
 function favRTs () {  
@@ -45,9 +55,10 @@ function favRTs () {
   });
 }
 
-// every 2 minutes, make and tweet a metaphor
-// wrapped in a try/catch in case Twitter is unresponsive, don't really care about error
-// handling. it just won't tweet.
+/*  Every 2 minutes, make and tweet a sentence
+ *  wrapped in a try/catch in case Twitter is unresponsive,
+ *  don't really care about error handling. it just won't tweet. 
+ */
 setInterval(function() {
   try {
     // makeSentence();
